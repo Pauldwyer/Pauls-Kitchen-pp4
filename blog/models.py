f7adec4ac62e18dfe134bd6488fa0ba89f1ab1c2
Pauldwyer <pauldwyer55@gmail.com>
2023-03-07 19:35:51 +0000

    set up custom user model, basic templates to test

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class CustomUser(AbstractUser):
+    pass
+    # add additional fields in here
+
+    def __str__(self):
+        return self.username
+
 
 # Create your models here.

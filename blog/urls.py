a50088470ac9c5f2142b3ec50ed4be4ef5e188fd
Pauldwyer <pauldwyer55@gmail.com>
2023-03-08 22:09:50 +0000

    built out base.html and wired up recipe list on home.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 
 from .views import SignUpView
 
 urlpatterns = [
+    path('', views.RecipeList.as_view(), name='home'),
     path("signup/", SignUpView.as_view(), name="signup"),
 ]

95029acc319d1de1e9aa57b2f996884615f1eea4
Pauldwyer <pauldwyer55@gmail.com>
2023-03-08 20:00:39 +0000

    add RecipeList view and add bootstrap to base.html

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.views import generic
 
 from .forms import CustomUserCreationForm
+from .models import Recipe
+
+
+class RecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-published_on')
+    template_name = 'home.html'
+    paginate_by = 6
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
-# Create your views here.
+

384ae630a89c1d32126fab4a74da327d9fb64664
Pauldwyer <pauldwyer55@gmail.com>
2023-03-09 14:39:26 +0000

    add recipe_detail view, url and html

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from django.views import generic
+from django.views import generic, View
 
 from .forms import CustomUserCreationForm
 from .models import Recipe
@@ -12,6 +12,27 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-published_on')
     template_name = 'home.html'
     paginate_by = 6
+
+
+class RecipeDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        # if recipe.likes.filter(id=self.request.user.id).exists():
+            # liked = True
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "liked": liked
+            }
+        )
 
 
 class SignUpView(CreateView):

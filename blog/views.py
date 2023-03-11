1e5654b389cc70444cbc0bf6fb987cc8d3a1ec03
Pauldwyer <pauldwyer55@gmail.com>
2023-03-11 13:11:27 +0000

    made changes to recipe_detail.html layout2

--- blog/views.py
+++ blog/views.py
@@ -21,7 +21,7 @@ class RecipeList(generic.ListView):
 class RecipeDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
-        recipe = get_object_or_404(queryset, slug=slug)
+        recipe = get_object_or_404(Recipe, slug=slug)
         comments = recipe.comments.filter(approved=True).order_by('created_on')
         liked = False
         # if recipe.likes.filter(id=self.request.user.id).exists():
